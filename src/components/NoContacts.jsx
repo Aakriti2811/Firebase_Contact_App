533f51f5327cbd41629048679793e5aff63a9b08
Aakriti <aakriti.kashyap7497@gmail.comgit config --global user.name Aakritigit config --global user.email aakriti.kashyap7497@gmail.com>
2024-09-13 13:45:10 +0530

    new

--- src/components/NoContacts.jsx
+++ src/components/NoContacts.jsx
@@ -5,7 +5,7 @@ const NoContacts = () => {
   return (
      <div className="absolute top-[50%] w-full flex flex-col items-center justify-center">
           <img src={contact} alt="contact  logo" className=" w-[60px]" />
-          <h1 className="text-gray-400 mt-3">No Contacts</h1>
+          <h1 className="text-gray-400 mt-3">Contact Not Found!</h1>
         </div> 
   )
 }
