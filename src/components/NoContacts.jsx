1e6fa729f239628331615dddea639d9edd20dced
Aakriti <aakriti.kashyap7497@gmail.comgit config --global user.name Aakritigit config --global user.email aakriti.kashyap7497@gmail.com>
2024-09-13 13:06:27 +0530

    new

--- src/components/NoContacts.jsx
+++ src/components/NoContacts.jsx
@@ -3,9 +3,9 @@ import contact from "../assets/Hands Contact.png";
 
 const NoContacts = () => {
   return (
-     <div className="absolute top-[50%] left-[45%] flex-col">
-          <img src={contact} alt="contact  logo" className=" w-[80px]" />
-          <h1 className="text-gray-400 mt-3">No Contact</h1>
+     <div className="absolute top-[50%] w-full flex flex-col items-center justify-center">
+          <img src={contact} alt="contact  logo" className=" w-[60px]" />
+          <h1 className="text-gray-400 mt-3">No Contacts</h1>
         </div> 
   )
 }
